5a2508a9cc6f011ec6325710e3ffb2290c7e94df
Md Sajid <sajidcse202024@gmail.com>
2026-06-30 18:18:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,22 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	const prices = document.querySelectorAll(".prices");
+    let total = 0;
+
+    prices.forEach(function (price) {
+        total += Number(price.textContent);
+    });
+
+    const row = document.createElement("tr");
+    const cell = document.createElement("td");
+
+    cell.colSpan = 2;
+    cell.textContent = total;
+
+    row.appendChild(cell);
+
+    document.querySelector("table").appendChild(row);
   
 };
 
